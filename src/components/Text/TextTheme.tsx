928211e835ba52c95cea3177800b5785d5d6231b
Mustak24 <162583970+Mustak24@users.noreply.github.com>
2025-05-31 23:19:52 +0530

    fix error in TextTheme component by using useTheme hook.

--- src/components/Text/TextTheme.tsx
+++ src/components/Text/TextTheme.tsx
@@ -1,10 +1,9 @@
-import { useContext } from "react";
 import { Text, TextProps } from "react-native";
-import { ThemeContext } from "../../Contexts/ThemeProvider";
+import { useTheme } from "../../Contexts/ThemeProvider";
 
 
 export default function TextTheme({style, children, numberOfLines}: TextProps): React.JSX.Element {
-    const {primaryColor: color} = useContext(ThemeContext);
+    const {primaryColor: color} = useTheme();
     
     return (
         <Text numberOfLines={numberOfLines}  style={[style, {color}]} >{children}</Text>
