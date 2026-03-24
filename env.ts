d475e592ff310eb2fc13f9b86c0c43d60f40e487
Mustak24 <mustak.coc786@gmail.com>
2026-03-24 18:31:27 +0530

    update privacy policy page URL

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.e_wallet.pixelProphet';
 
 
-export const PRIVACY_POLICY_URL = 'https://mustak24.github.io/Pixel-Wallet-privacy-policy/';
+export const PRIVACY_POLICY_URL = 'https://pixel-propx-solutions.github.io/privacy-policy-pages/pixel-wallet.html';
 
 
 export const GITHUB_URL = 'https://github.com/Mustak24/Pixel-Wallet';
